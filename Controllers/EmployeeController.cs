b0ef6455b263f82201be98ba3adb3a804b2b6bda
Mateen <Mateen@DESKTOP-S02B72F>
2023-09-24 22:44:26 +0500

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public string[] employee = { "MATEEN", "ALI", "AHSAN" };
+
+        [HttpGet]
+        public string[] GetEmployees()
+        {
+            return employee;
+        }
+
+        [HttpGet("{id}")]
+        public string GetEmployeeById(int id)
+        {
+           
+                return employee[id];
+            
+         }
+    }
+}
